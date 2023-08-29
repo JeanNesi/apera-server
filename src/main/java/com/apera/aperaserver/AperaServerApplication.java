3742a977cd5e3bce9994e4b2b4c6f2bf265ed2b6
Jean Carlos Nesi <jean.35322@alunosatc.edu.br>
2023-08-28 21:48:09 -0300

    first commit

---
+++ src/main/java/com/apera/aperaserver/AperaServerApplication.java
@@ -0,0 +1,13 @@
+package com.apera.aperaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AperaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AperaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apera/aperaserver/AperaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apera.aperaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AperaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
