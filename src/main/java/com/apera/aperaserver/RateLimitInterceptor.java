b20c020d1618178436d7aaa29ea0351d6b8a09a2
Oziel Silveira Junior <37221417+ozielsilveira@users.noreply.github.com>
2023-11-20 19:38:20 -0300

    add rate limit

---
+++ src/main/java/com/apera/aperaserver/RateLimitInterceptor.java
@@ -0,0 +1,45 @@
+package com.apera.aperaserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RateLimitInterceptor extends HandlerInterceptorAdapter {
+
+    private static final int MAX_REQUESTS_PER_HOUR = 10; // Ajuste conforme necessário
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    public RateLimitInterceptor(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String clientId = getClientId(request);
+        String requestCountKey = clientId + ":requests";
+
+        Long requests = redisTemplate.opsForValue().increment(requestCountKey, 1);
+        if (requests == 1) {
+            redisTemplate.expire(requestCountKey, 1, TimeUnit.HOURS);
+        }
+
+        if (requests > MAX_REQUESTS_PER_HOUR) {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            return false;
+        }
+
+        return true;
+    }
+
+    private String getClientId(HttpServletRequest request) {
+        return request.getRemoteAddr();
+    }
+}
